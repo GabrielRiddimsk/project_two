3032dcf8d31c7bc3495d2b4ac9b24a4c3f70e437
Pavel <gabrielriddimsk@gmail.com>
2025-03-24 16:51:38 +0700

    first version

---
+++ git dz.php
@@ -0,0 +1,9 @@
+<?php
+// create new project folder "GIT DZ" 
+// git --version
+// git config --global user.name Pavel
+// git config --global user.email gabrielriddimsk@gmail.com
+// git init
+// create .gitignore file
+// git add
+// git status
